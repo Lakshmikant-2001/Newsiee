989384120b04580bbc8247663d277a08670f87af
lakshmikant <lakshmikant2512@gmail.com>
2022-01-19 18:58:30 +0530

    add navbar open and close functionality

---
+++ js/common.js
@@ -0,0 +1,11 @@
+const hamBtn = document.querySelector("#ham-btn");
+const sideNavBar = document.querySelector("#side-nav");
+
+hamBtn.addEventListener("click",()=> {
+    if(sideNavBar.classList=="close"){
+        sideNavBar.classList.replace("close","open");
+    }
+    else{
+        sideNavBar.classList.replace("open","close");
+    }
+})
